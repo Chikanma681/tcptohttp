0ae65242331348410fa60592874d6e6d0a6d59b4
Chikanma Chika-Onuegbu <chikanmaonuegbu@ymail.com>
2026-01-05 23:51:33 -0700

    tcp to reader to channekl

--- main.go
+++ main.go
@@ -7,32 +7,52 @@ import (
 	"bytes"
 ) 
 
+
+func getLinesChannel(f io.ReadCloser)<-chan string {
+	out := make(chan string, 1)
+
+	go func(){
+		defer f.Close()
+		close(out)
+		str:= ""
+		for {
+			data := make([]byte,8)
+			n, err := f.Read(data)
+
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i:= bytes.IndexByte(data, '\n'); i != -1 {
+				str+= string(data[:i])
+				data=data[i+1:]
+				out <- str
+				str=""
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+}
 func main() {
-	f, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp",":42069")
 	if err != nil {
 		log.Fatal("error","error", err)
 	}
 
-	str:= ""
+	lines := getLinesChannel(f)
+
 	for {
-		data := make([]byte,8)
-		n, err := f.Read(data)
-
+		conn, err := listener.Accept()
 		if err != nil {
-			break
+			log.Fatal("error", "error", err)
 		}
-
-		data = data[:n]
-		if i:= bytes.IndexByte(data, '\n'); i != -1 {
-			str+= string(data[:i])
-			data=data[i+1:]
-			fmt.Printf("read:%s\n",str)
-			str=""
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("read: %s\n", line)
 		}
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read:%s\n", str)
 	}
 }
